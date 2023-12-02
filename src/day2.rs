ee34bc39def5e3a661f6d6636f3df92f8dc428e2
Mike Young <mikeyoung3k@gmail.com>
2023-12-02 09:50:52 +0000

    day 2

---
+++ src/day2.rs
@@ -0,0 +1,77 @@
+use std::cmp::max;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+const P1_DATUM: Game = Game{
+    id: 0,
+    blue: 14,
+    green: 13,
+    red: 12,
+};
+
+
+pub fn run(file: &str) -> (usize,usize)  {
+    let games = read_data(file);
+    let p1 = pt1(&games);
+    let p2 = pt2(&games);
+    return (p1,p2);
+}
+
+#[derive(Debug,Default)]
+struct Game {
+    id: usize,
+    blue: usize,
+    green: usize,
+    red: usize,
+}
+
+fn read_data(file: &str) -> Vec<Game> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(\d+) (\w+)").expect("Failed to compile regex");
+    }
+    std::fs::read_to_string(file)
+    .expect("File not found")
+    .lines()
+    .map(|x| {
+        let mut game = Game{..Default::default()};
+        if let Some((gid,gnums)) = x.split_once(":") {
+            game.id = gid.trim_start_matches(char::is_alphabetic).trim().parse::<usize>().unwrap();
+            for round in gnums.split(";") {
+                    for sequence in round.split(","){
+                    let cap = RE.captures(sequence).expect("Failed to match regex");
+                    match &cap[2] {
+                        "red" => {
+                            game.red = max(game.red, cap[1].parse::<usize>().expect("Failed to parse red"))
+                        }
+                        "blue" => {
+                            game.blue = max(game.blue, cap[1].parse::<usize>().expect("Failed to parse blue"))
+                        }
+                        "green" => {
+                            game.green = max(game.green, cap[1].parse::<usize>().expect("Failed to parse green"))
+                        }
+                        _ => panic!("Unknown colour")
+                    }
+                }
+            }
+        };
+        game
+    }).collect()
+}
+
+fn pt1(games: &Vec<Game>) -> usize{
+    let mut res = 0;
+    for game in games {
+        if game.red <= P1_DATUM.red &&
+        game.green <= P1_DATUM.green &&
+        game.blue   <=  P1_DATUM.blue {
+            res += game.id;
+        }
+    }
+    return res
+}
+
+fn pt2(games: &Vec<Game>) -> usize{
+   games.iter().map(|game| {
+    game.red * game.blue * game.green
+   }).sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@
 
 
 mod day1;
+mod day2;
 
 fn main() {
-    day1();
+    //day1();
+    day2();
 }
 
 fn day1() {
@@ -13,5 +15,12 @@ fn day1() {
     assert_eq!(test_res.0,142);
     assert_eq!(test_res.1,281);
     let res = day1::run("puzzle_input/day1.txt");
+    println!("{:?}", res);
+}
+fn day2() {
+    let test_res = day2::run("sample_data/day2.txt");
+    assert_eq!(test_res.0,8);
+    assert_eq!(test_res.1,2286);
+    let res = day2::run("puzzle_input/day2.txt");
     println!("{:?}", res);
 }
